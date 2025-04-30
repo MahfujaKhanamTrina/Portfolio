c9a19b11a942f9c02ca14bde26a2c2f5231f482c
MahfujaKhanamTrina <ma.trina1000@gmail.com>
2025-04-30 21:04:00 +0600

    add second

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Project from './Components/Project'
 import Contact from './Components/Contact'
 import Footer from './Components/Footer'
 import AboutMe from './Components/AboutMe'
+import { HashRouter } from 'react-router-dom'
 
 
 
 export default function App(){
   return(
     <>
+    <HashRouter>
     <div className="w-ful">
         {/* Navbar */}
         <div className="">
@@ -50,6 +52,8 @@ export default function App(){
             <Footer />
         </div>
     </div>
+
+    </HashRouter>
 </>
   )
 
